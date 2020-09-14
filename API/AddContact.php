9c52635252710c637ce820d51348173ec4b46dab
joeygiordano13 <joeyg@DESKTOP-IFUKP63.localdomain>
2020-09-13 21:23:43 -0400

    Add now rejects existing contact id

--- API/AddContact.php
+++ API/AddContact.php
@@ -19,25 +19,24 @@
 	} 
 	else
 	{
-		/*
-		$users = "SELECT username, password FROM users";
-		$result = $conn->query($users)
-		if ($userid > $result->num_rows)
-		{
-			echo "Key not present in user db";
-			returnWithError("userid: " . $userid);
-		}
-*/
-
+		// Check if userid is present in users table.
 		$primary_key = "SELECT * from users WHERE userid ='" . $userid . "'";
-		$result = $conn->query($primary_key)
+		$result = $conn->query($primary_key);
 		if (empty($result))
 		{
-			echo "Key not present in user db";
+			echo "Key not present in users table.";
 			returnWithError("userid: " . $userid);
 		}
 		
-		
+		// Reject new contact if userid already present in contacts table. 
+		$foreign_key = "SELECT * from contacts WHERE userid ='" . $userid . "'";
+		$result = $conn->query($foreign_key);
+		if (!empty($result))
+		{
+			echo "Key already present in contacts table.";
+			returnWithError("userid: " . $userid);
+		}
+
 		$sql = "INSERT into contacts (userid,firstname,lastname,phonenumber,email) VALUES (" . $userid . ",'" . $firstname . "','" . $lastname . "','" . $phonenumber . "','" . $email . "')";
 		//returnWithError($sql);
 		if($result = $conn->query($sql) != TRUE)
