359b76cb11ea500b021a790dfec4d1ff6a59324e
Joey Giordano <43688181+joeygiordano13@users.noreply.github.com>
2020-09-21 18:39:27 -0400

    Revert "Added lots of endpoint links in contact.js and login.js. These acts a…"

--- js/contacts.js
+++ js/contacts.js
@@ -22,39 +22,12 @@ document
         var newEmail = document.getElementById("newEmail").value;
         var newAddress = document.getElementById("newAddress").value;
 
-        // TODO: create addContact id in html
         document.getElementById("addContact").innerHTML = "";
-
-        var jsonPayload = '{"userid" : ' + userid + '"first name" : "' + newFirst + '", "last name" :  "' + newLast + '", "new   Phone" : "' + newPhone + '", "new email" : "' + newEmail + '"}';
-
-        //Rename this with AddContact API
-        var url = urlBase + "AddContact.php"; 
-
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        try
-        {
-
-        }
-    }
-
-    function editContact()
-    {
-        var newFirst = document.getElementById("newFirst").value;
-        var newLast = document.getElementById("newLast").value;
-        var newPhone = document.getElementById("newPhone").value;
-        var newEmail = document.getElementById("newEmail").value;
-        var newAddress = document.getElementById("newAddress").value;
-
-        // TODO: create editContact id in html
-        document.getElementById("editContact").innerHTML = "";
 
         var jsonPayload = '{"first name" : "' + newFirst + '", "last name" :  "' + newLast + '", "new   Phone" : "' + newPhone + '", "new email" : "' + newEmail + '"}';
 
-        //Rename this with AddContact API
-        var url = urlBase + "AddContact.php"; 
+        //Rename this with addcontact API
+        var url = urlBase;
 
         var xhr = new XMLHttpRequest();
         xhr.open("POST", url, true);
@@ -65,67 +38,20 @@ document
 
         }
     }
-
-    // waiting
-    function deleteContact()
-    {
-        // TODO: create deleteContact id in html
-        document.getElementById("deleteContact").innerHTML = "";
-
-        // WIP need to know json format
-        var jsonPayload;
-
-        //Rename this with DeleteContact API
-        var url = urlBase + "DeleteContact.php"; 
-
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        try
+        function search() 
         {
-
-        }
-    }
-
-    // Is this not needed due to api?
-    function search() 
-    {
-        var input, filter, ul, tr, a, i, txtValue;
-        input = document.getElementById("myInput"); // should this be "search"?
-        filter = input.value.toUpperCase();
-        tr = document.getElementById("myUL");
-        tr = ul.getElementsByTagName("li");
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("a")[0];
-            txtValue = a.textContent || a.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
+            var input, filter, ul, tr, a, i, txtValue;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            tr = document.getElementById("myUL");
+            tr = ul.getElementsByTagName("li");
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("a")[0];
+                txtValue = a.textContent || a.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
             }
         }
-    }
-
-    // check w/ backend to see if upper/lowercase matters
-    function searchAll()
-    {
-        var input;
-        input = document.getElementById("search");
-
-        // input = input.value.toLowerCase();
-
-        var jsonPayload = '{"search" : "' + input '"}';
-
-        //Rename this with SearchContactByName API
-        var url = urlBase + "SearchByAll.php"; 
-
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        try
-        {
-
-        }
-    }
--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 var userID = 0;
 var firstName = "";
 var lastName = "";
-var urlBase = "http://206.189.193.36/API";
+var url = "http://206.189.193.36/API/Login.php";
 
 function doLogin()
 {
@@ -11,68 +11,6 @@ function doLogin()
     var jsonPayload = JSON.stringify({username : usernameInput, password : passwordInput});
 
     var xhr = new XMLHttpRequest();
-    var url = urlBase + "/Login.php";
-
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-
-    /*
-    xhr.send(jsonPayload);
-    xhr.onreadystatechange = function()
-    {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
-        {
-            alert(xhr.responseText);
-        }
-    }
-    */
-    try
-	{
-        xhr.send(jsonPayload);
-        //xhr.onreadystatechange = alert(xhr.responseType);
-        var jsonObject = JSON.parse(xhr.response);
-        alert(jsonObject);
-        
-        
-		
-		userId = jsonObject.id;
-		
-		if( userId < 1 )
-		{
-            alert("login fail!");
-			//document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-			return;
-        }
-        
-        alert("login fail!");
-        return;
-
-        /*
-		firstName = jsonObject.firstName;
-		lastName = jsonObject.lastName;
-
-		saveCookie();
-	
-        window.location.href = "color.html";
-        */
-        
-	}
-	catch(err)
-	{
-        alert("error: " + err.message);
-		//document.getElementById("loginResult").innerHTML = err.message;
-    }
-}
-
-function doRegister()
-{
-    var usernameInput = document.getElementById("inputUsername").value;
-    var passwordInput = document.getElementById("inputPassword").value;
-
-    var jsonPayload = JSON.stringify({username : usernameInput, password : passwordInput});
-
-    var xhr = new XMLHttpRequest();
-    var url = urlBase + "/Login.php";
 
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
