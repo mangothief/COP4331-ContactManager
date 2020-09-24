635d1b881ff0ad974de588f1d94bb8c558cfe3fe
joeygiordano13 <joeyg@DESKTOP-IFUKP63.localdomain>
2020-09-23 21:55:41 -0400

    debug testAPI.

--- API/testAPI.php
+++ API/testAPI.php
@@ -1,4 +1,6 @@
 <?php
+    $inData = getRequestInfo();
+    
     $conn = new mysqli('localhost', 'root', '8C@UnIoOwUK2k7gZl%N9Mi', 'cookiebook');
 
     // check for connectivity issues
@@ -17,4 +19,29 @@
         $registerSQL = "INSERT into users(username,password) VALUES ('" . $username . "','" . $password . "')";
         echo $registerSQL;
     }
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
+	function returnWithInfo($searchResults)
+	{
+		$retValue = '{"results":[' . $searchResults . ']"}';
+		sendResultInfoAsJson($retValue);
+	}
+?>
+
 ?>
