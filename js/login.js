57d0924468c1a37b72b71d56cfb4d08488d6bc6a
unknown <AlexanderALongo@knights.ucf.edu>
2020-09-25 17:14:07 -0400

    Small update to login script

--- js/login.js
+++ js/login.js
@@ -27,8 +27,8 @@ function doLogin()
         {
             alert(`Response is \"${xhr.response}\"`);
             var jsonObject = JSON.parse(xhr.response);
-            alert(`jsonObject is \"${jsonObject}\"`);
-            userId = jsonObject.id;
+            userID = jsonObject.id;
+            alert(`id is \"${userID}\"`);
             if(userID == 0)
             {
                 alert(`Login fail`)
@@ -47,12 +47,15 @@ function doLogin()
         alert(`Network Error`);
     };
 
+    //Only needed for network testing
+    /*
     xhr.onprogress = function(event) { // triggers periodically
         // event.loaded - how many bytes downloaded
         // event.lengthComputable = true if the server sent Content-Length header
         // event.total - total number of bytes (if lengthComputable)
         alert(`Received ${event.loaded} of ${event.total}`);
     };
+    */
 
     alert("Finished xhr");
 }
