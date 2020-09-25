eabdbdf6886b563d130e8cfc0908af47a57d3d69
joeygiordano13 <joeyg@DESKTOP-IFUKP63.localdomain>
2020-09-25 15:33:39 -0400

    changed search results

--- API/SearchContacts.php
+++ API/SearchContacts.php
@@ -22,14 +22,14 @@
         $search = $inData["search"];
         // Cross-reference searched name with firstnames and lastnames in contacts.
         $sql = "SELECT contactid,firstname,lastname,email FROM contacts where firstname LIKE '%" . $search . "%' OR lastname LIKE '%" . $search . "%' OR email LIKE '%" . $search . "%' AND userid=" . $userid;
-        echo $sql;
+        //echo $sql;
         
         $result = $conn->query($sql);
         
         // Number of contacts we must search.
         $searchCount = $result->num_rows;
-        echo "\nnum rows:" . $searchCount . "\n";
-        //echo $searchCount;
+        //echo "\nnum rows:" . $searchCount . "\n";
+
         // Contacts left to search.
         if ($searchCount > 0)
         {
@@ -39,7 +39,7 @@
             {
                 echo "here\n";
                 $row = $result->fetch_assoc();
-                $thisJsonObject = '{"contactid":' . $row["contactid"] . '"}';
+                $thisJsonObject = '{"contactid":' . $row["contactid"] . '","' . $firstname . '","' . $lastname . '","' . $email . '"}';
                 
                 // Push json object onto array for matching contact
                 $searchResults .= $thisJsonObject;
