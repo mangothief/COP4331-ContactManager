75494f87129b7fb97d25b749554dd58ffe018e7e
unknown <AlexanderALongo@knights.ucf.edu>
2020-09-29 18:52:26 -0400

    added register

--- js/login.js
+++ js/login.js
@@ -59,6 +59,70 @@ function doLogin()
     //alert("Finished xhr");
 };
 
+function doRegister()
+{
+    var usernameInput = document.getElementById("inputUsername").value;
+    var passwordInput = document.getElementById("inputPassword").value;
+    var confirmPasswordInput = document.getElementById("inputConfirmPassword").value;
+
+    if(passwordInput != confirmPasswordInput)
+    {
+        document.getElementById("loginResult").innerHTML = `Passwords do not match`;
+        return;
+    }
+
+    var jsonPayload = JSON.stringify({username : usernameInput, password : passwordInput});
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    xhr.send(jsonPayload);
+
+    xhr.onload = function() {
+        //alert(`Loaded: ${xhr.status} ${xhr.response}`);
+        if(xhr.status == 500)
+        {
+            document.getElementById("loginResult").innerHTML = `500 internal server error`;
+        } 
+        else if(xhr.status == 200)
+        {
+            //alert(`Response is \"${xhr.response}\"`);
+            var jsonObject = JSON.parse(xhr.response);
+            userID = jsonObject.userid;
+            //alert(`id is \"${userID}\"`);
+            if(typeof jsonObject.error == 'undefined')
+            {
+                showLogin();
+                document.getElementById("loginResult").innerHTML = `${jsonObject.error}`;
+            }
+            else
+            {
+                document.getElementById("loginResult").innerHTML = `${jsonObject.error}`;
+            }
+        }
+        else
+        {
+            document.getElementById("loginResult").innerHTML = `Unspecified error: error code ${xhr.status}`;
+        }
+    };
+    xhr.onerror = function() { // only triggers if the request couldn't be made at all
+        document.getElementById("loginResult").innerHTML = `Network Error`;
+    };
+
+    //Only needed for network testing
+    /*
+    xhr.onprogress = function(event) { // triggers periodically
+        // event.loaded - how many bytes downloaded
+        // event.lengthComputable = true if the server sent Content-Length header
+        // event.total - total number of bytes (if lengthComputable)
+        alert(`Received ${event.loaded} of ${event.total}`);
+    };
+    */
+
+    //alert("Finished xhr");
+};
+
 function showRegister()
 {
     /*
